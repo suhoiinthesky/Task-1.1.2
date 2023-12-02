12fdcf47ede0daf1d76aebe32b0c8fe24c929d7d
suhoiinthesky <144448945+suhoiinthesky@users.noreply.github.com>
2023-12-03 01:00:29 +0300

    Delete src/test/test.sql

--- src/test/test.sql
+++
@@ -1,4 +0,0 @@
-insert into animal(animal_name, animal_age) values('cat', 2);
-insert into animal(animal_name, animal_age) values('dog', 3);
-insert into animal(animal_name, animal_age) values('mouse', 1);
-select * from animal;
